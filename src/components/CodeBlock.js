ece80a31303f6d32c76ee8c4c85174126b577480
Johnnie Thurston <johnniethurston@gmail.com>
2018-02-13 17:10:19 -0600

    a start

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { atomDark } from 'react-syntax-highlighter/styles/prism';
+import { createElement } from 'react-syntax-highlighter';
 
-export default ({ code }) => (
-	<SyntaxHighlighter language="javascript" style={atomDark} showLineNumbers>
-		{code}
-	</SyntaxHighlighter>
-);
+const customRenderer = ({ rows, stylesheet, useInlineStyles }) => {
+	return rows.map((row, idx) => {
+		console.log(row, idx);
+		// if __INPUT__, return an <input> tag, else:
+		return createElement({
+			node: rows[idx],
+			stylesheet,
+			useInlineStyles,
+			key: idx,
+		});
+	});
+};
+
+export default ({ code }) => {
+	return (
+		<SyntaxHighlighter language="javascript" style={atomDark} renderer={customRenderer} showLineNumbers>
+			{code}
+		</SyntaxHighlighter>
+	);
+};
