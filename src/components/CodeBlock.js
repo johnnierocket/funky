f6155a4bb04f941a8691cd1d0b9058e6fe24d75b
Johnnie Thurston <johnniethurston@gmail.com>
2018-02-13 17:31:12 -0600

    styling

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -2,12 +2,24 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { atomDark } from 'react-syntax-highlighter/styles/prism';
 import { createElement } from 'react-syntax-highlighter';
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+	background: transparent;
+	color: white;
+	font-size: 1rem;
+	border 1px solid white;
+	border-radius: 5px;
+	margin: 0 5px;
+	padding: 1px 5px;
+	outline: none;
+`;
 
 const customRenderer = ({ rows, stylesheet, useInlineStyles }) => {
 	return rows.map((row, idx) => {
 		return row.children.map((item, idx) => {
 			if (item.children && item.children[0].value === '__INPUT__') {
-				return <input key={idx} type="text" value="blah" />;
+				return <StyledInput key={idx} type="text" placeholder="?" size="" />;
 			}
 			return createElement({
 				node: item,
