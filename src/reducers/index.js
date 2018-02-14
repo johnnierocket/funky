f2578c6f60686075e05ee849845b05be57fe4833
Bradley Spaulding <bspaulding@spredfast.com>
2018-02-14 12:00:06 -0600

    root reducer combines user reducer, exports selectors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,19 @@
-import { combineReducers } from 'redux';
-import questionsReducer from './questionsReducer';
+import { combineReducers, compose } from 'redux';
+import * as questionsReducer from './questionsReducer';
+import * as userReducer from './userReducer';
 
 const rootReducer = combineReducers({
-  questionsReducer,
+	questionsReducer: questionsReducer.default,
+	user: userReducer.default,
 });
 
 export default rootReducer;
+
+export const getQuestionsState = state => state.questionsReducer;
+const getUserState = state => state.user;
+
+export const loggedIn = compose(userReducer.loggedIn, getUserState);
+export const userName = compose(userReducer.userName, getUserState);
+export const userId = compose(userReducer.userId, getUserState);
+export const userAvatarUrl = compose(userReducer.userAvatarUrl, getUserState);
+export const getTotalPoints = compose(questionsReducer.getTotalPoints, getQuestionsState);
