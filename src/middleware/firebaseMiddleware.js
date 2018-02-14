b045cc939fd2093de39a8318efa5ee9575b73b2f
Bradley Spaulding <bspaulding@spredfast.com>
2018-02-14 16:47:29 -0600

    Updated firebase middleware to save user info, sub to leaderboard changes

--- src/middleware/firebaseMiddleware.js
+++ src/middleware/firebaseMiddleware.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
-import { LOGIN, REHYDRATE_QUESTIONS } from '../constants/actionTypes';
-import { getQuestionsState, getTotalPoints, loggedIn } from '../reducers/index';
+import { LOGIN, REHYDRATE_QUESTIONS, UPDATE_LEADERBOARD } from '../constants/actionTypes';
+import { getQuestionsState, getTotalPoints, loggedIn, userAvatarUrl, userName } from '../reducers/index';
 
 const uid = () => firebase.auth().currentUser.uid;
-const refName = () => `/users/${uid()}/questions`;
-const pointsRefName = () => `/leaderboard/${uid()}/points`;
+const questionsRefName = () => `/users/${uid()}/questions`;
+const userLeaderboardRefName = () => `/leaderboard/${uid()}`;
+const userInfoRefName = () => `/users/${uid()}/meta`;
 const saveToFirebase = store => {
 	const state = store.getState();
 	if (!loggedIn(state)) {
@@ -12,17 +13,27 @@ const saveToFirebase = store => {
 	}
 	firebase
 		.database()
-		.ref(refName())
+		.ref(questionsRefName())
 		.set(JSON.stringify(getQuestionsState(state)));
 	firebase
 		.database()
-		.ref(pointsRefName())
-		.set(getTotalPoints(state));
+		.ref(userLeaderboardRefName())
+		.set({ name: userName(state), avatarUrl: userAvatarUrl(state), points: getTotalPoints(state) });
 };
+const updateUserInfo = store => {
+	const state = store.getState();
+	if (loggedIn(state)) {
+		firebase
+			.database()
+			.ref(userInfoRefName())
+			.set({ name: userName(state), avatarUrl: userAvatarUrl(state) });
+	}
+};
+
 const rehydrateQuestions = async store => {
 	const result = await firebase
 		.database()
-		.ref(refName())
+		.ref(questionsRefName())
 		.once('value');
 	const payload = result.val();
 	if (payload) {
@@ -32,11 +43,22 @@ const rehydrateQuestions = async store => {
 	}
 };
 
-export default store => next => action => {
-	next(action);
-	if (action.type === LOGIN) {
-		rehydrateQuestions(store);
-	} else {
-		saveToFirebase(store);
-	}
+const subscribeToLeaderboard = store => {
+	firebase
+		.database()
+		.ref(`/leaderboard`)
+		.on('value', snapshot => store.dispatch({ type: UPDATE_LEADERBOARD, payload: snapshot.val() }));
+};
+
+export default store => {
+	subscribeToLeaderboard(store);
+	return next => action => {
+		next(action);
+		if (action.type === LOGIN) {
+			rehydrateQuestions(store);
+			updateUserInfo(store);
+		} else {
+			saveToFirebase(store);
+		}
+	};
 };
