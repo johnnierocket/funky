174f49d2c9e8f25403c01269f8b83d2c19b9b5bf
Johnnie Thurston <johnniethurston@gmail.com>
2018-02-13 18:56:29 -0600

    autosize input

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,40 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { atomDark } from 'react-syntax-highlighter/styles/prism';
 import { createElement } from 'react-syntax-highlighter';
 import styled from 'styled-components';
+import AutosizeInput from 'react-input-autosize';
 
-const StyledInput = styled.input`
-	background: transparent;
-	color: white;
-	font-size: 1rem;
-	border 1px solid white;
-	border-radius: 5px;
-	margin: 0 5px;
-	padding: 1px 5px;
-	outline: none;
+const StyledAutosizeInput = styled(AutosizeInput)`
+	input {
+		background: transparent;
+		outline: none;
+		padding: 1px 5px;
+		margin: 5px;
+		border-radius: 5px;
+		border: 1px solid #444444;
+		font-size: 1rem;
+		color: white;
+		font-family: 'Fira Code';
+		text-align: center;
+	}
 `;
 
-const customRenderer = ({ rows, stylesheet, useInlineStyles }) => {
-	return rows.map((row, idx) => {
-		return row.children.map((item, idx) => {
-			if (item.children && item.children[0].value === '__INPUT__') {
-				return <StyledInput key={idx} type="text" placeholder="?" size="" />;
-			}
-			return createElement({
-				node: item,
-				stylesheet,
-				useInlineStyles,
-				key: idx,
+export default class CustomSyntaxHighlighter extends Component {
+	state = {
+		input: '',
+	};
+
+	handleInput = e => {
+		this.setState({
+			input: e.target.value,
+		});
+	};
+
+	customRenderer = ({ rows, stylesheet, useInlineStyles }) => {
+		return rows.map((row, idx) => {
+			return row.children.map((item, idx) => {
+				if (item.children && item.children[0].value === '__INPUT__') {
+					return (
+						<StyledAutosizeInput
+							autoFocus
+							key={idx}
+							type="text"
+							onChange={this.handleInput}
+							value={this.state.input}
+							placeholder="???"
+						/>
+					);
+				}
+				return createElement({
+					node: item,
+					stylesheet,
+					useInlineStyles,
+					key: idx,
+				});
 			});
 		});
-	});
-};
+	};
 
-export default ({ code }) => {
-	return (
-		<SyntaxHighlighter language="javascript" style={atomDark} renderer={customRenderer} showLineNumbers>
-			{code}
-		</SyntaxHighlighter>
-	);
-};
+	render() {
+		return (
+			<SyntaxHighlighter language="javascript" style={atomDark} renderer={this.customRenderer} showLineNumbers>
+				{this.props.code}
+			</SyntaxHighlighter>
+		);
+	}
+}
