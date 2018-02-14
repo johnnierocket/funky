44885636e296b776939c08e4096f7e59ace7f614
Johnnie Thurston <johnniethurston@gmail.com>
2018-02-14 17:32:28 -0600

    flex stuff

--- src/App.js
+++ src/App.js
@@ -26,17 +26,52 @@ import lose5 from './sounds/lose5.mp3';
 import lose6 from './sounds/lose6.mp3';
 
 const Root = styled.div`
-	width: 50%;
-	min-width: 800px;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
+	display: flex;
+	position: relative;
+	flex-direction: column;
 `;
 
 const StyledLinearProgress = styled(LinearProgress)`
-	margin-bottom: 40px !important;
-	background-color: #ffffff !important;
+	background-color: rgba(229, 229, 229, 0.76) !important;
+	height: 40px !important;
+	margin: 0 0 30px 0 !important;
+`;
+
+const Header = styled.div`
+	background-color: #6abcfb;
+	color: #fff;
+	h1 {
+		margin: 10px 15px;
+	}
+`;
+
+const YellowAccentBar = styled.div`
+	background-color: #ffd33f;
+	height: 5px;
+`;
+
+const RedAccentBar = styled.div`
+	background-color: #e84733;
+	height: 5px;
+`;
+
+const StyledRaisedButton = styled(RaisedButton)`
+	width: 200px;
+	align-self: center;
+`;
+
+const ContentContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin-top: 40px;
+`;
+
+const SideContainer = styled.div`
+	width: 20%;
+`;
+
+const CenterContainer = styled.div`
+	width: 55%;
 `;
 
 const style = {
@@ -112,7 +147,6 @@ class App extends Component {
 		const { loggedIn, questionId, questionsCompleted, totalPoints } = this.props;
 		const { correctSubmission, input, error } = this.state;
 		const exercise = exercises[questionId];
-		const instructions = `#${questionId} ${exercise.title}`;
 		const questionPreviouslyAnswered = questionsCompleted.includes(questionId);
 		const progressPercent = questionsCompleted.length / Object.keys(exercises).length * 100;
 		const winSoundsArray = [win1, win2, win3];
@@ -121,52 +155,65 @@ class App extends Component {
 		const randLose = loseSoundsArray[Math.floor(Math.random() * loseSoundsArray.length)];
 		return (
 			<Root>
+				<Header>
+					<h1>Funky</h1>
+					<YellowAccentBar />
+					<RedAccentBar />
+				</Header>
+
+				<StyledLinearProgress mode="determinate" value={progressPercent} color="#6cc93d" />
+				{/* <PointCounter points={totalPoints} /> */}
 				{loggedIn ? (
 					<UserInfo name={this.props.userName} avatarUrl={this.props.avatarUrl} />
 				) : (
-					<RaisedButton label="Login with GitHub" onClick={this.props.login} />
+					<StyledRaisedButton label="Login with GitHub" onClick={this.props.login} />
 				)}
-				<StyledLinearProgress mode="determinate" value={progressPercent} color="#ff4081" />
-				<PointCounter points={totalPoints} />
-				<Instructions text={instructions} />
-				<CodeBlock
-					code={exercise.display}
-					input={input}
-					onChange={this.onInputChange}
-					onKeyPress={this.handleKeyPress}
-					showLineNumbers={true}
-				/>
-				<SpinningVinyl labelColor="green" textColor="yellow" />
-				{error && (
-					<div>
-						<h1>Hmm... not quite.</h1>
-						<CodeBlock code={error} showLineNumbers={false} children={<span>Your</span>} />
-					</div>
-				)}
-				<RaisedButton
-					label="Go Back"
-					style={style}
-					onClick={this.previousQuestion}
-					disabled={!(questionId - 1)}
-				/>
-				<RaisedButton
-					label="Attempt Answer"
-					style={style}
-					backgroundColor="#ffa500"
-					labelColor="#ffffff"
-					onClick={this.validateResponse}
-					disabled={questionPreviouslyAnswered}
-				/>
-				<RaisedButton
-					label="Next Question"
-					style={style}
-					secondary={true}
-					onClick={this.nextQuestion}
-					disabled={!questionPreviouslyAnswered || questionId === exercises.length}
-				/>
-				{correctSubmission && <h1>Correct!</h1>}
-				{correctSubmission && <PlaySound src={randWin} />}
-				{error && <PlaySound src={randLose} />}
+				<ContentContainer>
+					<SideContainer />
+					<CenterContainer>
+						<Instructions questionId={questionId} text={exercise.title} />
+						<CodeBlock
+							code={exercise.display}
+							input={input}
+							onChange={this.onInputChange}
+							onKeyPress={this.handleKeyPress}
+							showLineNumbers={true}
+						/>
+						<SpinningVinyl labelColor="green" textColor="yellow" />
+						{error && (
+							<div>
+								<h1>Hmm... not quite.</h1>
+								<CodeBlock code={error} showLineNumbers={false} children={<span>Your</span>} />
+							</div>
+						)}
+						<RaisedButton
+							label="Go Back"
+							style={style}
+							onClick={this.previousQuestion}
+							disabled={!(questionId - 1)}
+						/>
+						<RaisedButton
+							label="Attempt Answer"
+							style={style}
+							backgroundColor="#ffa500"
+							labelColor="#ffffff"
+							onClick={this.validateResponse}
+							disabled={questionPreviouslyAnswered}
+						/>
+						<RaisedButton
+							label="Next Question"
+							style={style}
+							secondary={true}
+							onClick={this.nextQuestion}
+							disabled={!questionPreviouslyAnswered || questionId === exercises.length}
+						/>
+						{correctSubmission && <h1>Correct!</h1>}
+						{correctSubmission && <PlaySound src={randWin} />}
+						{error && <PlaySound src={randLose} />}
+					</CenterContainer>
+					<SideContainer />
+				</ContentContainer>
+				<SideContainer />
 			</Root>
 		);
 	}
--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -2,9 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledText = styled.div`
-	text-align: center;
+	text-align: left;
 	font-size: 1.5em;
-	margin-bottom: 20px;
+	margin-bottom: 40px;
 `;
 
-export default ({ text }) => <StyledText>{text}</StyledText>;
+const Situation = styled.div`
+	font-size: 2rem;
+	margin-bottom: 10px;
+`;
+
+export default ({ questionId, text }) => (
+	<StyledText>
+		<Situation>Situation {questionId}</Situation>
+		{text}
+	</StyledText>
+);
